219d6fbddbaad1a8fa133a0f9d61d79b2e197a2b
Thekkekl <42657228+Thekkekl@users.noreply.github.com>
2018-09-11 17:27:13 -0600

    Add files via upload

--- Invoice Total/Invoice Total/Form1.Designer.cs
+++ Invoice Total/Invoice Total/Form1.Designer.cs
@@ -29,31 +29,32 @@
         private void InitializeComponent()
         {
             this.printDialog1 = new System.Windows.Forms.PrintDialog();
-            this.btnCalculation = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
-            this.txtSubtotal = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.invoiceSubtotal = new System.Windows.Forms.TextBox();
+            this.txtDiscountPercent = new System.Windows.Forms.TextBox();
+            this.txtDiscountAmount = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
             this.lblSubtotal = new System.Windows.Forms.Label();
             this.lblDiscountPercent = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.lblDiscountAmount = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // printDialog1
             // 
             this.printDialog1.UseEXDialog = true;
             // 
-            // btnCalculation
+            // btnCalculate
             // 
-            this.btnCalculation.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCalculation.Location = new System.Drawing.Point(134, 316);
-            this.btnCalculation.Name = "btnCalculation";
-            this.btnCalculation.Size = new System.Drawing.Size(107, 30);
-            this.btnCalculation.TabIndex = 2;
-            this.btnCalculation.Text = "&Calculate";
-            this.btnCalculation.UseVisualStyleBackColor = true;
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(134, 316);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(107, 30);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
             // 
             // btnExit
             // 
@@ -65,44 +66,46 @@
             this.btnExit.TabIndex = 3;
             this.btnExit.Text = "E&xit";
             this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // txtSubtotal
             // 
-            this.txtSubtotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSubtotal.Location = new System.Drawing.Point(245, 68);
-            this.txtSubtotal.Name = "txtSubtotal";
-            this.txtSubtotal.Size = new System.Drawing.Size(230, 24);
-            this.txtSubtotal.TabIndex = 1;
+            this.invoiceSubtotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.invoiceSubtotal.Location = new System.Drawing.Point(245, 68);
+            this.invoiceSubtotal.Name = "txtSubtotal";
+            this.invoiceSubtotal.Size = new System.Drawing.Size(230, 24);
+            this.invoiceSubtotal.TabIndex = 1;
+            this.invoiceSubtotal.TextChanged += new System.EventHandler(this.txtSubtotal_TextChanged);
             // 
-            // textBox2
+            // txtDiscountPercent
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(245, 116);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(230, 24);
-            this.textBox2.TabIndex = 5;
-            this.textBox2.TabStop = false;
+            this.txtDiscountPercent.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDiscountPercent.Location = new System.Drawing.Point(245, 116);
+            this.txtDiscountPercent.Name = "txtDiscountPercent";
+            this.txtDiscountPercent.ReadOnly = true;
+            this.txtDiscountPercent.Size = new System.Drawing.Size(230, 24);
+            this.txtDiscountPercent.TabIndex = 5;
+            this.txtDiscountPercent.TabStop = false;
             // 
-            // textBox3
+            // txtDiscountAmount
             // 
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(245, 171);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.ReadOnly = true;
-            this.textBox3.Size = new System.Drawing.Size(230, 24);
-            this.textBox3.TabIndex = 7;
-            this.textBox3.TabStop = false;
+            this.txtDiscountAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDiscountAmount.Location = new System.Drawing.Point(245, 171);
+            this.txtDiscountAmount.Name = "txtDiscountAmount";
+            this.txtDiscountAmount.ReadOnly = true;
+            this.txtDiscountAmount.Size = new System.Drawing.Size(230, 24);
+            this.txtDiscountAmount.TabIndex = 7;
+            this.txtDiscountAmount.TabStop = false;
             // 
-            // textBox4
+            // txtTotal
             // 
-            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(245, 222);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.ReadOnly = true;
-            this.textBox4.Size = new System.Drawing.Size(230, 24);
-            this.textBox4.TabIndex = 9;
-            this.textBox4.TabStop = false;
+            this.txtTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotal.Location = new System.Drawing.Point(245, 222);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(230, 24);
+            this.txtTotal.TabIndex = 9;
+            this.txtTotal.TabStop = false;
             // 
             // lblSubtotal
             // 
@@ -114,6 +117,7 @@
             this.lblSubtotal.TabIndex = 0;
             this.lblSubtotal.Text = "&Subtotal:";
             this.lblSubtotal.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblSubtotal.Click += new System.EventHandler(this.lblSubtotal_Click);
             // 
             // lblDiscountPercent
             // 
@@ -126,48 +130,51 @@
             this.lblDiscountPercent.Text = "Discount Percent:";
             this.lblDiscountPercent.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // label3
+            // lblDiscountAmount
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(85, 171);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(122, 18);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "DiscountAmount:";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblDiscountAmount.AutoSize = true;
+            this.lblDiscountAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiscountAmount.Location = new System.Drawing.Point(85, 171);
+            this.lblDiscountAmount.Name = "lblDiscountAmount";
+            this.lblDiscountAmount.Size = new System.Drawing.Size(122, 18);
+            this.lblDiscountAmount.TabIndex = 6;
+            this.lblDiscountAmount.Text = "DiscountAmount:";
+            this.lblDiscountAmount.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblDiscountAmount.Click += new System.EventHandler(this.lblDiscountAmount_Click);
             // 
-            // label4
+            // lblTotal
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(85, 225);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(41, 18);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "Total";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(85, 225);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(41, 18);
+            this.lblTotal.TabIndex = 8;
+            this.lblTotal.Text = "Total";
+            this.lblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblTotal.Click += new System.EventHandler(this.lblTotal_Click);
             // 
             // frmInvoiceTotal
             // 
-            this.AcceptButton = this.btnCalculation;
+            this.AcceptButton = this.btnCalculate;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnExit;
             this.ClientSize = new System.Drawing.Size(564, 470);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblDiscountAmount);
             this.Controls.Add(this.lblDiscountPercent);
             this.Controls.Add(this.lblSubtotal);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.txtSubtotal);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtDiscountAmount);
+            this.Controls.Add(this.txtDiscountPercent);
+            this.Controls.Add(this.invoiceSubtotal);
             this.Controls.Add(this.btnExit);
-            this.Controls.Add(this.btnCalculation);
+            this.Controls.Add(this.btnCalculate);
             this.Name = "frmInvoiceTotal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Invoice Total";
+            this.Load += new System.EventHandler(this.frmInvoiceTotal_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -176,16 +183,16 @@
         #endregion
 
         private System.Windows.Forms.PrintDialog printDialog1;
-        private System.Windows.Forms.Button btnCalculation;
+        private System.Windows.Forms.Button btnCalculate;
         private System.Windows.Forms.Button btnExit;
-        private System.Windows.Forms.TextBox txtSubtotal;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox invoiceSubtotal;
+        private System.Windows.Forms.TextBox txtDiscountPercent;
+        private System.Windows.Forms.TextBox txtDiscountAmount;
+        private System.Windows.Forms.TextBox txtTotal;
         private System.Windows.Forms.Label lblSubtotal;
         private System.Windows.Forms.Label lblDiscountPercent;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblDiscountAmount;
+        private System.Windows.Forms.Label lblTotal;
     }
 }
 
